16b9ff50d28dda5c54ca48debdbc56a42036298c
fares <fm@ekelda.no>
2019-01-18 10:03:40 +0100

    fixed disabled buttons

--- src/UI/CardContainer.js
+++ src/UI/CardContainer.js
@@ -9,16 +9,16 @@ import FemaleIcon from './icons/femaleIcon'
 
 const styles = {
     buttonBlue: {
-        backgroundColor: 'blue',
+        backgroundColor: 'blue !important',
     },
     buttonRed: {
-        backgroundColor: 'red'
+        backgroundColor: 'red !important'
     },
     buttonGreen:{
-        backgroundColor: 'green'
+        backgroundColor: 'green !important'
     },
     buttonYellow:{
-        backgroundColor: 'yellow'
+        backgroundColor: 'yellow !important'
     },
     textBlack:{
         color:'black'
@@ -41,44 +41,91 @@ function CardContainer({
     classes,
 
 }) {
-       const verticalMargin=10 + 40 * Math.random();
-        return (
-            <div style={{margin: `10px 10px`,width: "calc(100%*(1/10))"}}>
-                {color==='blue' ?
-                    <Button variant="contained"
-                            className={classes.buttonBlue}
-                            classes={{disabled: classes.buttonBlue}}gi
-                            //style={{backgroundColor:'blue'}}
-                            onClick={whenClicked}
-                            disabled={!pubPile}
-                    > <h2 className={classes.textWhite}/*style={{color:'white'}}*/> <MaleIcon/> {numb}</h2> </Button> :
-                     color==='red' ?
-                         <Button variant="contained"
-                                 className={classes.buttonRed}
-                                 classes={{disabled: classes.buttonRed}}
-                             //style={{backgroundColor:'red',}}
-                                 onClick={whenClicked}
-                                 disabled={!pubPile}
-                         >  <h2 className={classes.textWhite}/*style={{color:'white'}}*/> <MaleIcon/> {numb}</h2> </Button> :
-                         color==='yellow' ?
-                            <Button /*style={{backgroundColor:'yellow',}}*/
-                                    className={classes.buttonYellow}
-                                    classes={{disabled: classes.buttonYellow}}
-                                    variant="contained"
-                                    onClick={whenClicked}
-                                    disabled={!pubPile}
-                            >  <h2 className={classes.textBlack}/*style={{color:'black'}}*/> <FemaleIcon/> {numb}</h2> </Button> :
-                             color === 'green' ?
-                                 <Button //style={{backgroundColor:'green',}}
-                                         variant="contained"
-                                         className={classes.buttonGreen}
-                                         classes={{disabled: classes.buttonGreen}}
-                                         onClick={whenClicked}
-                                         disabled={!pubPile} >  <h2 className={classes.textWhite}/*style={{color:'white'}}*/> <FemaleIcon/> {numb}</h2> </Button> :
-                                 <Button variant="contained" onClick={whenClicked} disabled={!pubPile} >  {numb} </Button>
-                }
-            </div>
-        )
+    const verticalMargin=10 + 40 * Math.random();
+
+    let buttonClasses, textClassName;
+
+    switch (color) {
+        case "blue":
+            buttonClasses = {root: classes.buttonBlue, disabled: classes.buttonBlue};
+            textClassName = classes.textWhite;
+            break;
+        case "red":
+            buttonClasses = {root: classes.buttonRed, disabled: classes.buttonRed};
+            textClassName = classes.textWhite;
+            break;
+        case "yellow":
+            buttonClasses = {root: classes.buttonYellow, disabled: classes.buttonYellow};
+            textClassName = classes.textBlack;
+            break;
+        case "green":
+            buttonClasses = {root: classes.buttonGreen, disabled: classes.buttonGreen};
+            textClassName = classes.textWhite;
+            break;
+        default:
+            buttonClasses = {};
+            textClassName = '';
+            break;
+    }
+
+    return (
+        <div style={{margin: `10px 10px`,width: "calc(100%*(1/10))"}}>
+            <Button
+                variant="contained"
+                classes={buttonClasses}
+                onClick={whenClicked}
+                disabled={!pubPile}
+            >
+                <h2 className={textClassName}>
+                    {sex === 'M' && <MaleIcon/>}
+                    {sex === 'F' && <FemaleIcon/>}
+                    {!!numb && numb}
+                </h2>
+
+                {!numb && numb}
+
+            </Button>
+            {/*{color==='blue' ?*/}
+                {/*<Button*/}
+                    {/*variant="contained"*/}
+                    {/*classes={{root: classes.buttonBlue, disabled: classes.buttonBlue}}*/}
+                    {/*onClick={whenClicked}*/}
+                    {/*disabled={!pubPile}*/}
+                {/*>*/}
+                    {/*<h2 className={classes.textWhite}>*/}
+                        {/*<MaleIcon/> {numb}*/}
+                    {/*</h2>*/}
+
+                {/*</Button>*/}
+
+                {/*: color==='red' ?*/}
+                     {/*<Button*/}
+                         {/*variant="contained"*/}
+                         {/*classes={{root: classes.buttonRed, disabled: classes.buttonRed}}*/}
+                         {/*onClick={whenClicked}*/}
+                         {/*disabled={!pubPile}*/}
+                     {/*>*/}
+                         {/*<h2>*/}
+                             {/*<MaleIcon/> {numb}*/}
+                         {/*</h2>*/}
+                     {/*</Button>*/}
+                    {/*: color==='yellow' ?*/}
+                        {/*<Button/}
+                                {/*classes={{root: classes.buttonYellow, disabled: classes.buttonYellow}}*/}
+                                {/*variant="contained"*/}
+                                {/*onClick={whenClicked}*/}
+                                {/*disabled={!pubPile}*/}
+                        {/*>  <h2 className={classes.textBlack}> <FemaleIcon/> {numb}</h2> </Button> :*/}
+                         {/*color === 'green' ?*/}
+                             {/*<Button //style={{backgroundColor:'green',}}*/}
+                                     {/*variant="contained"*/}
+                                     {/*classes={{root: classes.buttonGreen, disabled: classes.buttonGreen}}*/}
+                                     {/*onClick={whenClicked}*/}
+                                     {/*disabled={!pubPile} >  <h2> <FemaleIcon/> {numb}</h2> </Button> :*/}
+                             {/*<Button variant="contained" onClick={whenClicked} disabled={!pubPile} >  {numb} </Button>*/}
+            {/*}*/}
+        </div>
+    )
 }
 
 CardContainer = withStyles(styles)(CardContainer)
